35675f795cf76828681f965e3c1ae57bc0b3a1dc
Cristian Ospina <crisospina523@gmail.com>
2019-05-11 22:58:27 -0500

    Updates

---
+++ precache-manifest.98a84248f4b5215181f162f030da725a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e94afdcf49211bb134be7a1a16cc8f",
+    "url": "/cotizador-autos/index.html"
+  },
+  {
+    "revision": "b51548f139c45b2caa56",
+    "url": "/cotizador-autos/static/css/main.28374886.chunk.css"
+  },
+  {
+    "revision": "67886796a2dfac0801a8",
+    "url": "/cotizador-autos/static/js/2.8c14a617.chunk.js"
+  },
+  {
+    "revision": "b51548f139c45b2caa56",
+    "url": "/cotizador-autos/static/js/main.787ed4b5.chunk.js"
+  },
+  {
+    "revision": "740c189f558540436e41",
+    "url": "/cotizador-autos/static/js/runtime~main.0702e234.js"
+  }
+]);
